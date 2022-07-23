f813f61140ace2271601f20fb90d0eb1601b0031
Shane Mulligan <mullikine@gmail.com>
2022-07-23 18:15:39 +1200

    Sat 23 Jul 2022 18:15:39

---
+++ .org-brain-data.el
@@ -0,0 +1,6 @@
+(setq org-brain-pins
+      '(
+        ("index" "ai" "24cd5649-8182-428d-91dd-03f852ab8d10")
+        ("index" "machine learning" "25ed80d3-03ef-4fad-8f08-353adc9076ac")
+        "index"
+        ))
